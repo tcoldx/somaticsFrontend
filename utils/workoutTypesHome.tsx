b42a808c8ba68af8568dd7ab98b7a6f56f34ad8d
tcoldx <t.tredis@gmail.com>
2023-11-11 02:03:06 -0800

    design changes and added workout types section

---
+++ utils/workoutTypesHome.tsx
@@ -0,0 +1,24 @@
+export const bodyPartTypes = [
+  { name: "Chest", id: 1 },
+  { name: "Abs", id: 2 },
+  { name: "Arms", id: 3 },
+  { name: "Full Body", id: 4 },
+  { name: "Glutes", id: 5 },
+  { name: "Legs", id: 6 },
+  { name: "Shoulders", id: 7 },
+];
+
+export const sectionTypes = [
+  { name: "Featured", id: 1 },
+  { name: "Workouts", id: 2 },
+  { name: "Programs", id: 3 },
+];
+
+export const durationTypes = [
+  { name: "15", id: 1 },
+  { name: "30", id: 2 },
+  { name: "45", id: 3 },
+  { name: "60", id: 4 },
+  { name: "90", id: 5 },
+  { name: "120", id: 6 },
+];
